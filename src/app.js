3f08fbdf43ddd0da4ff7ade0195c01d3dc14f1f7
Phebueno <fcbgomes@gmail.com>
2023-04-18 14:40:26 -0300

    Configurações iniciais de server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";  
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+// Criação do servidor
+const app = express();
+
+// Configurações
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+// Setup do Banco de Dados
+let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message));
+
+// Deixa o app escutando, à espera de requisições
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
