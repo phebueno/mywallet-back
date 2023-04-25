b38cc272f063832c2e8b793150e7f082b51220f5
Phebueno <fcbgomes@gmail.com>
2023-04-25 14:12:22 -0300

    chore:ajuste de envio de dados para login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -27,14 +27,14 @@ export async function signin(req, res){
     try {
       const user = await db.collection("users").findOne({ email });
       if (!user) res.status(422).send("Usuário não cadastrado");
+      
       if (!bcrypt.compareSync(password, user.password))
         res.status(401).send("Senha incorreta.");
   
       //Criação de sessão
       const token = uuid();
-  
       await db.collection("sessions").insertOne({ userId: user._id, token });
-      res.status(200).send(token);
+      res.status(200).send({user:user.name, token});
     } catch (error) {
       console.log(error.message);
     }
